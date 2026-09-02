c6f4880850b0e13647360ef5c5b83bd16821611a
Tulio Tagliaferri <265392628+tjtags@users.noreply.github.com>
2026-09-02 12:25:47 -0400

    fix: twitter-image must declare route config, not re-export it

--- src/app/markets/[id]/twitter-image.tsx
+++ src/app/markets/[id]/twitter-image.tsx
@@ -1,1 +1,6 @@
-export { default, runtime, revalidate, size, contentType } from "./opengraph-image";
+export { default } from "./opengraph-image";
+
+export const runtime = "nodejs";
+export const revalidate = 60;
+export const size = { width: 1200, height: 630 };
+export const contentType = "image/png";
